9b44b1bb5926f787beecf4fe824fdbad5ae9bda1
Yakshesh Gupta <yaksheshgupta59@gmail.com>
2023-03-15 20:17:53 +0530

    first commit

---
+++ src/component/home.js
@@ -0,0 +1,21 @@
+import '../CSS/home.css';
+
+function Home() {
+    return (
+        <>
+                <div className='grid-container'>
+                    <div className='grid-element'>
+                        <a href='/' className='text1'></a>
+                    </div>
+                    <div className='grid-element'>
+                        <a href='/' className='text2'></a>
+                    </div>
+                    <div className='grid-element'>
+                        <a href='/'className='text3'></a>
+                    </div>
+                </div>
+        </>
+    );
+}
+
+export default Home;
