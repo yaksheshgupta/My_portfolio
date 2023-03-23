7f2425865627b0f0f6898e986c309b11fb8ce8e6
Yakshesh Gupta <yaksheshgupta59@gmail.com>
2023-03-24 00:38:56 +0530

    minor changes

--- src/component/home.js
+++ src/component/home.js
@@ -3,15 +3,17 @@ import { Link } from 'react-router-dom';
 function Home() {
     return (
         <>
-            <div className='flex-container'>
-                <div>
-                    <Link to='/about' className='text1' ></Link>
-                </div>
-                <div>
-                    <Link to='/work' className='text2' ></Link>
-                </div>
-                <div>
-                    <Link to='/contact' className='text3'></Link>
+            <div className='pic-home'>
+                <div className='flex-container'>
+                    <div>
+                        <Link to='/about' className='text1' ></Link>
+                    </div>
+                    <div>
+                        <Link to='/work' className='text2' ></Link>
+                    </div>
+                    <div>
+                        <Link to='/contact' className='text3'></Link>
+                    </div>
                 </div>
             </div>
         </>
